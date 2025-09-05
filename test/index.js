1e28671d445af8c3d6156323c3321968eb92c140
Abhises Poudyal <abhisespoudyal@gmail.com>
2025-09-05 11:05:25 +0545

    adding

--- test/index.js
+++ test/index.js
@@ -1,87 +1,86 @@
-// import createChat_test from "./createChat_test.js";
-// import createChimeChat_test from "./createChimeChat_test.js";
-// import fetchRecentMessages_test from "./fetchRecentMessages_test.js";
-// import fetchUserChats_test from "./fetchUserChats_test.js";
-// import archiveChat_test from "./archiveChat_test.js";
-// import expireOldChats_test from "./expireOldChats_test.js";
-// import autoArchiveChats_test from "./autoArchiveChats_test.js";
-// import updateChatSubscriptionFlag_test from "./updateChatSubscriptionFlag_test.js";
-// import updateChatMode_test from "./updateChatMode_test.js";
-// import setChatRole_test from "./setChatRole_test.js";
-// import createGroupChat_test from "./createGroupChat_test.js";
-// import updateChatMetadata_test from "./updateChatMetadata_test.js";
-// import updateNotificationSettings_test from "./updateNotificationSettings_test.js";
-// import reactToMessage_test from "./reactToMessage_test.js";
-// import sendMessage_test from "./sendMessage_test.js";
-// import sendVoiceMessage_test from "./sendVoiceMessage_test.js";
-// import linkPollToMessage_test from "./linkPollToMessage_test.js";
-// import sendMixedMessage_test from "./sendMixedMessage_test.js";
-// import validateMessageLength_test from "./validateMessageLength_test.js";
-// import filterBannedWords_test from "./filterBannedWords_test.js";
-// import runEditMessageTests from "./editMessage_test.js";
-// import runMarkMessageReadTests from "./markMessageRead_test.js";
-// import runUnreadCountTests from "./getUnreadCount_test.js";
-// import sendProductRecommendation_test from "./sendProductRecommendation_test.js";
-// import lockMessageReplies_test from "./lockMessageReplies_test.js";
-// import attachTaskToMessage_test from "./attachTaskToMessage_test.js";
-// import sendVirtualGift_test from "./sendVirtualGift_test.js";
-// import sendTip_test from "./sendTip_test.js";
-// import sendPaidMedia_test from "./sendPaidMedia_test.js";
-// import updateChatAccess_test from "./updateChatAccess_test.js";
-// import createEventChat_test from "./createEventChat_test.js";
-// import updateMembershipTiers_test from "./updateMembershipTiers_test.js";
-// import sendExclusiveContent_test from "./sendExclusiveContent_test.js";
-// import startChatTrial_test from "./startChatTrial_test.js";
-// import parseChatMessage_test from "./parseChatMessage_test.js";
-// import storeChatMessage_test from "./testStoreChatMessage.js";
-// import handleIncomingMessage_test from "./handleIncomingMessage_test.js";
-// import runJoinChatTests from "./runJoinChatTests.js";
-// import runUpgradeMembershipTests from "./runUpgradeMembershipTests.js";
-// import runDeleteMessageTests from "./deleteMessage_test.js";
-// import runFlagMessageUrgentTests from "./flagMessageUrgent_test.js";
+import createChat_test from "./createChat_test.js";
+import createChimeChat_test from "./createChimeChat_test.js";
+import fetchRecentMessages_test from "./fetchRecentMessages_test.js";
+import fetchUserChats_test from "./fetchUserChats_test.js";
+import archiveChat_test from "./archiveChat_test.js";
+import expireOldChats_test from "./expireOldChats_test.js";
+import autoArchiveChats_test from "./autoArchiveChats_test.js";
+import updateChatSubscriptionFlag_test from "./updateChatSubscriptionFlag_test.js";
+import updateChatMode_test from "./updateChatMode_test.js";
+import setChatRole_test from "./setChatRole_test.js";
+import createGroupChat_test from "./createGroupChat_test.js";
+import updateChatMetadata_test from "./updateChatMetadata_test.js";
+import updateNotificationSettings_test from "./updateNotificationSettings_test.js";
+import reactToMessage_test from "./reactToMessage_test.js";
+import sendMessage_test from "./sendMessage_test.js";
+import sendVoiceMessage_test from "./sendVoiceMessage_test.js";
+import linkPollToMessage_test from "./linkPollToMessage_test.js";
+import sendMixedMessage_test from "./sendMixedMessage_test.js";
+import validateMessageLength_test from "./validateMessageLength_test.js";
+import filterBannedWords_test from "./filterBannedWords_test.js";
+import runEditMessageTests from "./editMessage_test.js";
+import runMarkMessageReadTests from "./markMessageRead_test.js";
+import runUnreadCountTests from "./getUnreadCount_test.js";
+import sendProductRecommendation_test from "./sendProductRecommendation_test.js";
+import lockMessageReplies_test from "./lockMessageReplies_test.js";
+import attachTaskToMessage_test from "./attachTaskToMessage_test.js";
+import sendVirtualGift_test from "./sendVirtualGift_test.js";
+import sendTip_test from "./sendTip_test.js";
+import sendPaidMedia_test from "./sendPaidMedia_test.js";
+import updateChatAccess_test from "./updateChatAccess_test.js";
+import createEventChat_test from "./createEventChat_test.js";
+import updateMembershipTiers_test from "./updateMembershipTiers_test.js";
+import sendExclusiveContent_test from "./sendExclusiveContent_test.js";
+import startChatTrial_test from "./startChatTrial_test.js";
+import parseChatMessage_test from "./parseChatMessage_test.js";
+import storeChatMessage_test from "./testStoreChatMessage.js";
+import handleIncomingMessage_test from "./handleIncomingMessage_test.js";
+import runJoinChatTests from "./runJoinChatTests.js";
+import runUpgradeMembershipTests from "./runUpgradeMembershipTests.js";
+import runDeleteMessageTests from "./deleteMessage_test.js";
+import runFlagMessageUrgentTests from "./flagMessageUrgent_test.js";
 import runPinMessageTests from "./pinMessage_test.js";
 export {
-  // createChat_test,
-  // createChimeChat_test,
-  // fetchRecentMessages_test,
-  // fetchUserChats_test,
-  // archiveChat_test,
-  // expireOldChats_test,
-  // autoArchiveChats_test,
-  // updateChatSubscriptionFlag_test,
-  // updateChatMode_test,
-  // setChatRole_test,
-  // createGroupChat_test,
-  // updateChatMetadata_test,
-  // updateNotificationSettings_test,
-  // reactToMessage_test,
-  // sendMessage_test,
-  // sendVoiceMessage_test,
-  // linkPollToMessage_test,
-  // sendMixedMessage_test,
-  // validateMessageLength_test,
-  // filterBannedWords_test,
-  // runEditMessageTests,
-
-  // runMarkMessageReadTests,
-  // runUnreadCountTests,
-  // sendProductRecommendation_test,
-  // lockMessageReplies_test,
-  // attachTaskToMessage_test,
-  // sendVirtualGift_test,
-  // sendTip_test,
-  // sendPaidMedia_test,
-  // updateChatAccess_test,
-  // createEventChat_test,
-  // updateMembershipTiers_test,
-  // sendExclusiveContent_test,
-  // startChatTrial_test,
-  // parseChatMessage_test,
-  // storeChatMessage_test,
-  // handleIncomingMessage_test,
-  // runJoinChatTests,
-  // runUpgradeMembershipTests,
-  // runDeleteMessageTests,
-  // runFlagMessageUrgentTests,
+  createChat_test,
+  createChimeChat_test,
+  fetchRecentMessages_test,
+  fetchUserChats_test,
+  archiveChat_test,
+  expireOldChats_test,
+  autoArchiveChats_test,
+  updateChatSubscriptionFlag_test,
+  updateChatMode_test,
+  setChatRole_test,
+  createGroupChat_test,
+  updateChatMetadata_test,
+  updateNotificationSettings_test,
+  reactToMessage_test,
+  sendMessage_test,
+  sendVoiceMessage_test,
+  linkPollToMessage_test,
+  sendMixedMessage_test,
+  validateMessageLength_test,
+  filterBannedWords_test,
+  runEditMessageTests,
+  runMarkMessageReadTests,
+  runUnreadCountTests,
+  sendProductRecommendation_test,
+  lockMessageReplies_test,
+  attachTaskToMessage_test,
+  sendVirtualGift_test,
+  sendTip_test,
+  sendPaidMedia_test,
+  updateChatAccess_test,
+  createEventChat_test,
+  updateMembershipTiers_test,
+  sendExclusiveContent_test,
+  startChatTrial_test,
+  parseChatMessage_test,
+  storeChatMessage_test,
+  handleIncomingMessage_test,
+  runJoinChatTests,
+  runUpgradeMembershipTests,
+  runDeleteMessageTests,
+  runFlagMessageUrgentTests,
   runPinMessageTests,
 };
